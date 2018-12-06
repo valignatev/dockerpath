da7d226fa8b81eadb2221d6e4397088bddf069aa
Valentin Ignatev <valentignatev@gmail.com>
2018-12-06 10:40:14 +0300

    something draft

--- dockerpath/__init__.py
+++ dockerpath/__init__.py
@@ -1,4 +1,7 @@
+import os
 import sys
+import tarfile
+import tempfile
 
 import docker
 
@@ -6,13 +9,25 @@ import docker
 def setup(container_name):
     client = docker.from_env()
     container = client.containers.get(container_name)
-    container_sys_path = with_container_info(
-        container.name,
-        parse_remote_path(
-            remote_sys_path(container)
-        )
-    )
-    sys.path += container_sys_path
+    container_sys_path = parse_remote_path(remote_sys_path(container))
+    remote_paths_to_prepend = []
+    for path in filter(None, container_sys_path):
+        remote_path_list = path.split('/')
+        local_path = os.path.join(*(['.dockerpath', container.name] + remote_path_list[:-1]))
+        with tempfile.NamedTemporaryFile() as tmp:
+            try:
+                data, _ = container.get_archive(path)
+            except docker.errors.NotFound as e:
+                print(e.explanation)
+                continue
+            else:
+                for chunk in data.stream():
+                    tmp.write(chunk)
+            tmp.seek(0)
+            with tarfile.open(mode='r', fileobj=tmp) as tar:
+                tar.extractall(local_path)
+        remote_paths_to_prepend.append(os.path.join(local_path, remote_path_list[-1]))
+    sys.path = remote_paths_to_prepend + sys.path
 
 
 def remote_sys_path(container):
