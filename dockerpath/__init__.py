b7558719019a1f5a46fe141fa7375d6ff9a6dbe2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-15 03:49:27 +0000

    Adding tarfile member sanitization to extractall()

--- dockerpath/__init__.py
+++ dockerpath/__init__.py
@@ -25,7 +25,26 @@ def setup(container_name):
                     tmp.write(chunk)
             tmp.seek(0)
             with tarfile.open(mode='r', fileobj=tmp) as tar:
-                tar.extractall(local_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, local_path)
         remote_paths_to_prepend.append(os.path.join(local_path, remote_path_list[-1]))
     sys.path = remote_paths_to_prepend + sys.path
 
